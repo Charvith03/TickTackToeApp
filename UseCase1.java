40511092520f4ce660d2230ef17bac85b0c36ba8
Charvith <charvith03@icloud.com>
2026-04-20 13:38:01 +0530

    UC1 work

--- UseCase1.java
+++ UseCase1.java
@@ -1,20 +1,18 @@
-#include <iostream>
-using namespace std;
+public class Board {
+    public static void main(String[] args) {
 
-int main() {
-    char board[3][3];
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            board[i][j] = '-';
+        char board[][] = new char[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
         }
     }
-    cout << "Tic-Tac-Toe Board:\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    return 0;
 }
